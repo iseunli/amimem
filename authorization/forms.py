3c8ded5b650ea86034bb5838436fd2eea6c89c07
Saltanat <yensebayevas@gmail.com>
2022-05-13 06:19:39 +0600

    Ну все остальное

---
+++ authorization/forms.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.models import User
+from django import forms
+
+class EditAccountForm(UserChangeForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_login = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    is_superuser = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
+    is_stuff = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
+    is_active = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
+    date_joined = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model=User
+        fields =(
+                 'first_name',
+                 'last_name',
+                 'username',
+                 'email',
+                 'is_superuser',
+                 'is_stuff',
+                 'is_active',
+                 'date_joined',
+                 'last_login')
+
